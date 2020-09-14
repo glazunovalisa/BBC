96f8cc40b13b8a5cbeddb18ea55bbbe241d3aec4
glazunovalisa <glazunovsalisa1@gmail.com>
2020-09-14 15:13:20 +0200

    First commit

---
+++ BBC/UnitTest1.cs
@@ -0,0 +1,27 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace BBC
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.bbc.com");
+            }
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.bbc.com");
+            }
+        }
+    }
+}
