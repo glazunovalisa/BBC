b082902de9f429c0a519107c387e32d2d6589d2f
glazunovalisa <glazunovsalisa1@gmail.com>
2020-09-18 17:45:02 +0200

    Latest Commit

--- BBC/UnitTest1.cs
+++ BBC/UnitTest1.cs
@@ -85,7 +85,7 @@ namespace BBC
 
         [Fact]
         [Obsolete]
-        public void SubmitQuestion()
+        public void SubmitQuestionWithoutName()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
@@ -149,5 +149,205 @@ namespace BBC
                 Assert.Equal("Name can't be blank", validationErrors[0].Text);
             }
         }
+
+
+        [Fact]
+        [Obsolete]
+        public void SubmitQuestionWithoutAgreeingToTerms()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                IWebElement agreeCookies = driver.FindElement(By.XPath(".//button[@id='bbcprivacy-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(agreeCookies)).Click();
+                IWebElement yesCookies = driver.FindElement(By.XPath(".//button[@id='bbccookies-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(yesCookies)).Click();
+
+                IWebElement newsElement = driver.FindElement(By.XPath(".//div[@id='orb-nav-links']//a[contains(text(), 'News')]"));
+                newsElement.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                IWebElement maybeLater = driver.FindElement(By.XPath(".//button[@class='sign_in-exit']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(maybeLater)).Click();
+
+                IWebElement coronavirusElement = driver.FindElement(By.XPath(".//li[contains(@class, 'wide-menuitem-container')]//span[contains(text(), 'Coronavirus')]"));
+                coronavirusElement.Click();
+
+                IWebElement coronavirusStoriesElement = driver.FindElement(By.XPath(".//li[contains(@class, 'secondary')]//a"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(coronavirusStoriesElement)).Click();
+
+
+
+                IWebElement shareStory = driver.FindElement(By.XPath(".//a[contains(@href, '10725415')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", shareStory);
+                shareStory.Click();
+
+                IWebElement submitStory = driver.FindElement(By.XPath(".//button[contains(text(), 'Submit')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", submitStory);
+
+                driver.FindElement(By.XPath(".//textarea[@placeholder='Tell us your story. ']")).SendKeys("blabla");
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Name']")).SendKeys("blabla");
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Email address']")).SendKeys("randombutvalidemail@gmail.com");
+
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Contact number ']")).SendKeys("1");
+
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Location ']")).SendKeys("Chioggia");
+
+
+                driver.FindElement(By.XPath(".//span[@class='checkbox__text']//p[contains(text(), '16')]")).Click();
+
+                //driver.FindElement(By.XPath(".//span[@class='checkbox__text']//p[contains(text(), 'accept')]")).Click();
+
+               
+
+                submitStory.Submit();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                string pageName = driver.Title;
+
+                var validationErrors = driver.FindElements(By.XPath(".//div[@class='input-error-message']"));
+
+                Assert.Equal(expectegPageTitle, pageName);
+                Assert.Single(validationErrors);
+                Assert.Equal("must be accepted", validationErrors[0].Text);
+
+            }
+        }
+
+
+        [Fact]
+        [Obsolete]
+        public void SubmitEmptyFormForQuestion()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                IWebElement agreeCookies = driver.FindElement(By.XPath(".//button[@id='bbcprivacy-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(agreeCookies)).Click();
+                IWebElement yesCookies = driver.FindElement(By.XPath(".//button[@id='bbccookies-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(yesCookies)).Click();
+
+                IWebElement newsElement = driver.FindElement(By.XPath(".//div[@id='orb-nav-links']//a[contains(text(), 'News')]"));
+                newsElement.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                IWebElement maybeLater = driver.FindElement(By.XPath(".//button[@class='sign_in-exit']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(maybeLater)).Click();
+
+                IWebElement coronavirusElement = driver.FindElement(By.XPath(".//li[contains(@class, 'wide-menuitem-container')]//span[contains(text(), 'Coronavirus')]"));
+                coronavirusElement.Click();
+
+                IWebElement coronavirusStoriesElement = driver.FindElement(By.XPath(".//li[contains(@class, 'secondary')]//a"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(coronavirusStoriesElement)).Click();
+
+
+
+                IWebElement shareStory = driver.FindElement(By.XPath(".//a[contains(@href, '10725415')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", shareStory);
+                shareStory.Click();
+
+                IWebElement submitStory = driver.FindElement(By.XPath(".//button[contains(text(), 'Submit')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", submitStory);
+
+
+
+                submitStory.Submit();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                string pageName = driver.Title;
+
+                var validationErrors = driver.FindElements(By.XPath(".//div[@class='input-error-message']"));
+
+                Assert.Equal(expectegPageTitle, pageName);
+                Assert.Equal(4, validationErrors.Count);
+                Assert.Equal("can't be blank", validationErrors[0].Text);
+                Assert.Equal("Name can't be blank", validationErrors[1].Text);
+                Assert.Equal("must be accepted", validationErrors[2].Text);
+                Assert.Equal("must be accepted", validationErrors[3].Text);
+
+            }
+        }
+
+        [Fact]
+        [Obsolete]
+        public void SubmitQuestionUsingInvalidEmail()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                IWebElement agreeCookies = driver.FindElement(By.XPath(".//button[@id='bbcprivacy-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(agreeCookies)).Click();
+                IWebElement yesCookies = driver.FindElement(By.XPath(".//button[@id='bbccookies-continue-button']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(yesCookies)).Click();
+
+                IWebElement newsElement = driver.FindElement(By.XPath(".//div[@id='orb-nav-links']//a[contains(text(), 'News')]"));
+                newsElement.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                IWebElement maybeLater = driver.FindElement(By.XPath(".//button[@class='sign_in-exit']"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(maybeLater)).Click();
+
+                IWebElement coronavirusElement = driver.FindElement(By.XPath(".//li[contains(@class, 'wide-menuitem-container')]//span[contains(text(), 'Coronavirus')]"));
+                coronavirusElement.Click();
+
+                IWebElement coronavirusStoriesElement = driver.FindElement(By.XPath(".//li[contains(@class, 'secondary')]//a"));
+                wait.Until(ExpectedConditions.ElementToBeClickable(coronavirusStoriesElement)).Click();
+
+
+
+                IWebElement shareStory = driver.FindElement(By.XPath(".//a[contains(@href, '10725415')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", shareStory);
+                shareStory.Click();
+
+                IWebElement submitStory = driver.FindElement(By.XPath(".//button[contains(text(), 'Submit')]"));
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", submitStory);
+
+                driver.FindElement(By.XPath(".//textarea[@placeholder='Tell us your story. ']")).SendKeys("blabla");
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Name']")).SendKeys("blabla");
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Email address']")).SendKeys("randomnotvalidemailgmail.com");
+
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Contact number ']")).SendKeys("1");
+
+
+                driver.FindElement(By.XPath(".//input[@placeholder='Location ']")).SendKeys("Chioggia");
+
+
+                driver.FindElement(By.XPath(".//span[@class='checkbox__text']//p[contains(text(), '16')]")).Click();
+
+                driver.FindElement(By.XPath(".//span[@class='checkbox__text']//p[contains(text(), 'accept')]")).Click();
+
+
+
+                submitStory.Submit();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+                string pageName = driver.Title;
+
+                var validationErrors = driver.FindElements(By.XPath(".//div[@class='input-error-message']"));
+
+                Assert.Equal(expectegPageTitle, pageName);
+                Assert.Single(validationErrors);
+                Assert.Equal("Email address is invalid", validationErrors[0].Text);
+               
+
+            }
+        }
+
     }
 }
