6f80093fad0e4372b92625f70ed4dc5dd0f36909
glazunovalisa <glazunovsalisa1@gmail.com>
2020-09-19 14:49:31 +0200

    Latest commit

---
+++ BBC/Pages/NewsPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace BBC.Pages
+{
+    public class NewsPage
+    {
+
+        private readonly IWebDriver Driver;
+
+
+        public NewsPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebElement NewsElement => Driver.FindElement(By.XPath(".//div[@id='orb-nav-links']//a[contains(text(), 'News')]"));
+        private IWebElement NameOfHeadlineArticle => Driver.FindElement(By.XPath(".//div[@data-entityid='container-top-stories#1']//div[contains(@class, 'block@m')]//a[contains(@class, 'anchor')]"));
+
+
+
+
+        public void ClickOnNewsElement()
+        {
+            NewsElement.Click();
+        }
+
+        public string GetNameOfHeadlineArticle()
+        {
+            return NameOfHeadlineArticle.Text;
+        }
+    }
+}
---
+++ BBC/Tests/CheckArticleNameTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using BBC.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace BBC.Tests
+{
+    public class CheckArticleNameTests
+    {
+        private const string HomeUrl = "https://www.bbc.com";
+        private string expectedNameOfHeadlineArticle = "PLACEHOLDER FOR EXPECTED ARTICLE NAME'";
+        private string expectedSecondaryArticleName = "UK government under pressure over lack of tests";
+        private int amountOfSecondaryArticle = 15;
+        
+
+        [Fact]
+        public void CheckNameOfHeadLineArticle()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Arrange 
+                driver.Navigate().GoToUrl(HomeUrl);
+                var getNewsPage = new NewsPage(driver);
+                var getBasePage = new BasePage(driver);
+
+                //Act
+                getNewsPage.ClickOnNewsElement();
+                getBasePage.ImplicitWait(50);
+
+                //Assert
+                Assert.Equal(expectedNameOfHeadlineArticle, getNewsPage.GetNameOfHeadlineArticle());
+
+
+            }
+        }
+
+        [Fact]
+        public void CheckSecondaryArticlesName()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                IWebElement newsElement = driver.FindElement(By.XPath(".//div[@id='orb-nav-links']//a[contains(text(), 'News')]"));
+                newsElement.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+                ReadOnlyCollection<IWebElement> secondaryArticles
+                    = driver.FindElements(By.XPath(".//div[contains(@class, 'top-stories--international')]//h3[contains(@class, 'gel-pica-bold')]"));
+                int amountOfSecondaryArticles = secondaryArticles.Count;
+                Assert.Equal(expectedSecondaryArticleName, secondaryArticles[1].Text);
+                Assert.Equal(amountOfSecondaryArticle, amountOfSecondaryArticles);
+
+
+            }
+        }
+    }
+}
