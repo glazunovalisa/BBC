263b570528c5a1cf8c8a241604318c7b07da564b
glazunovalisa <glazunovsalisa1@gmail.com>
2020-09-19 18:22:53 +0200

    Added CookiesPage

---
+++ BBC/Pages/CookiesPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace BBC.Pages
+{
+    public class CookiesPage
+    {
+        private readonly IWebDriver Driver;
+
+
+        public CookiesPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebElement ChangesInCookiesButton => Driver.FindElement(By.XPath(".//button[@id='bbcprivacy-continue-button']"));
+        private IWebElement IAgreeToCookiesButton => Driver.FindElement(By.XPath(".//button[@id='bbccookies-continue-button']"));
+
+        public IWebElement OKChangesInCookiesButton()
+        {
+            return ChangesInCookiesButton;
+        }
+
+        public void ClickOnOKToChangesInCookiesButton()
+        {
+            ChangesInCookiesButton.Click();
+        }
+
+        public IWebElement AgreeToCookiesButton()
+        {
+            return IAgreeToCookiesButton;
+        }
+
+        public void ClickOnIAgreeToCookiesButton()
+        {
+            IAgreeToCookiesButton.Click();
+        }
+
+        [Obsolete]
+        public void AgreeToAllTheCookies()
+        {
+            var getBasePage = new BasePage(Driver);
+            var getCookiesPage = new CookiesPage(Driver);
+
+            getBasePage.WaitForElementToBeClickable(50, getCookiesPage.OKChangesInCookiesButton());
+            getCookiesPage.ClickOnOKToChangesInCookiesButton();
+            getBasePage.WaitForElementToBeClickable(50, getCookiesPage.AgreeToCookiesButton());
+            getCookiesPage.ClickOnIAgreeToCookiesButton();
+        }
+    }
+}
